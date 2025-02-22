97a94ef61e4125722b5e72a5cb3b29776803bd3d
zoyirr <zoyir6171@gmail.com>
2025-02-22 15:45:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let lyuboynom=prompt("string type da malumot kiriting");
+let lyuboynom2=+prompt("number type da malumot kiriting");
+let lyuboynom3=prompt("Boolean type da malumot kiriting");
+let lyuboynom4=+prompt("bigint type da malumot kiriting");
+
+
+confirm("hammasi tog'rimi");
+
+
+alert(`Siz kiritgan ${lyuboynom} so'zi ${typeof lyuboynom} typega tegishli \nSiz kiritgan ${lyuboynom2} raqam ${typeof lyuboynom2} typega tegishli
+Siz kiritgan ${lyuboynom3} raqam ${typeof lyuboynom3} typega tegishli \nSiz kiritgan ${lyuboynom4} raqam ${typeof lyuboynom4} typega tegishli
+    `)
